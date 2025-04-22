87c0a868c2935e735108631a177457d979138c38
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-04-22 08:55:27 -0300

    fix: handle incorrect username or password in session creation

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,30 @@
-import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError";
+import { Request, Response } from "express";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: process.env.AUTH_SECRET })
+    const { username, password } = request.body;
+
+    const fakeUser = {
+      id: 1,
+      username: "cristiano",
+      password: "123456"
+    };
+
+    if(username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Username or password incorrect", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id)
+    })
+
+    return response.json({ token })
   }
 }
 
